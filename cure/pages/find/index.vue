11230f3831aa06ff7ba860b1ec77592203b48da2
lc <1969956387@qq.com>
2022-03-11 23:30:02 +0800

    feat: smallCode

---
+++ cure/pages/find/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="find">
+		发现
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ cure/pages/home/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="home">
+		<view class="search">
+			<u-search shape="square" height="35" bgColor="#fff" v-model="search" placeholder="搜索问题" placeholderColor="#c6c6c4" :actionStyle="{ color: '#fff' }" ></u-search>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				search: ""
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.search {
+		padding: 40rpx;
+		background-color: #6ab5ee;
+	}
+</style>
